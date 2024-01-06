78ae58d1c2f100c66b10cfc7e7e31244352e874a
Dibya Jyoti Borah <dibya.borah@taazaa.com>
2024-01-06 16:16:02 +0530

    Putting the 50Challenge pdf & understanding recursion

--- prob15.js
+++ prob15.js
@@ -1,2 +1,26 @@
-// This is a demo comment
-// This is another demo comment to check commits
+// Print the first 10 Fibonacci numbers without using recursion.
+// Reminder:
+// F(0) = 0
+// F(1) = 1
+// F(n) = F(n-1) + F(n-2)
+
+// Understanding RECURSION
+
+// const factorial = (num) => {
+//   if(num<=1){
+//     return 1;
+//   }
+//   return num * factorial(num - 1);
+// };
+
+// The recursion pattern is explained below 
+// All the functions gets opened one by one
+// Let say: 5
+// factorial(5) => 5 * factorial(4)
+//              => 5 * (4 * factorial(3))
+//              => 5 * 4 * (3 * factorial(2))
+//              => 5 * 4 * 3 * (2 * factorial(1))
+//              => 5 * 4 * 3 * 2 * (1) => Solution
+
+// const result = factorial(5);
+// console.log(result);
