372c165809d6ba9e991de6f952711c52ab881c10
dibborah <dibyajyotiborah469@gmail.com>
2024-01-07 15:23:01 +0530

    prob16file was made in Typescript as .ts file completing the fibbonacci nth term problem using recursion

--- prob16.ts
+++ prob16.ts
@@ -1,11 +1,11 @@
-"use strict";
 // Create a function that will find the nth Fibonacci number using recursion.
 // Reminder:
 // F(0) = 0
 // F(1) = 1
 // F(n) = F(n-1) + F(n-2)
-// Object.defineProperty(exports, "__esModule", { value: true });
+
 // Finding the first 10 Fibbonacci number using recursion
+
 // let f_0 = 0;
 // console.log(f_0);
 // let f_1 = 1;
@@ -20,17 +20,24 @@
 //     }
 //     return findFibonnaci();
 // };
+
 // findFibonnaci();
+
 // Main Question
+
 // Finding the nth fibbonacci number using recursion
-var findNthFibbonacci = function (num) {
-    if (num === 0) {
-        return 0;
-    }
-    if (num === 1) {
-        return 1;
-    }
-    return findNthFibbonacci(num - 1) + findNthFibbonacci(num - 2);
+
+const findNthFibbonacci = (num: number): number => {
+  if (num === 0) {
+    return 0;
+  }
+  if (num === 1) {
+    return 1;
+  }
+  return findNthFibbonacci(num - 1) + findNthFibbonacci(num - 2);
 };
-var result = findNthFibbonacci(12);
+
+const result = findNthFibbonacci(0);
 console.log(result);
+
+export {};
