72790745a278a64f722575ca29aadc0acffe92e7
dibborah <dibyajyotiborah469@gmail.com>
2024-01-24 13:25:21 +0530

    completed getDistinctElements problem

--- prob28b.js
+++ prob28b.js
@@ -1,12 +1,9 @@
 // #27b
-
 // Create a function that will receive an array of
 // numbers as argument and will return a new
 // array with distinct elements
-
 // var ar = getDistinctElements([1, 2, 3, 6, -1, 2, 9, 7, 10, -1, 100]);
 // console.log(ar);
-
 // function getDistinctElements(ar) {
 //   let ar2: any = [];
 //   var lastIndex = ar.length - 1;
@@ -17,7 +14,6 @@
 //   }
 //   return ar2;
 // }
-
 // function isInArray(n, ar, fromIndex, toIndex) {
 //   for (var i = fromIndex; i <= toIndex; i++) {
 //     if (ar[i] === n) {
@@ -26,26 +22,23 @@
 //   }
 //   return false;
 // }
-
-const getDistinctElements = (ar) => {
-  let ar2: any = [];
-  const lastIndex = ar.length;
-  for (let i = 0; i < lastIndex; i++) {
-    if (!isInArray(ar[i], ar, i + 1, lastIndex)) {
-      ar2.push(ar[i]);
+var getDistinctElements = function (ar) {
+    var ar2 = [];
+    var lastIndex = ar.length;
+    for (var i = 0; i < lastIndex; i++) {
+        if (!isInArray(ar[i], ar, i + 1, lastIndex)) {
+            ar2.push(ar[i]);
+        }
     }
-  }
-  return ar2;
+    return ar2;
 };
-
-const isInArray = (n, ar, fromIndex, lastIndex) => {
-  for (let i = fromIndex; i < lastIndex; i++) {
-    if (ar[i] === n) {
-      return true;
+var isInArray = function (n, ar, fromIndex, lastIndex) {
+    for (var i = fromIndex; i < lastIndex; i++) {
+        if (ar[i] === n) {
+            return true;
+        }
     }
-  }
-  return false;
+    return false;
 };
-
 var ar = getDistinctElements([1, 2, 3, 6, -1, 2, 9, 7, 10, -1, 100]);
 console.log(ar);
