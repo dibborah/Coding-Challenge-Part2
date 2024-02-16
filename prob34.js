8c904fc00d87a0d9d28e8211d777c1fabafdae24
dibborah <dibyajyotiborah469@gmail.com>
2024-02-16 13:23:41 +0530

    refined sum of number in string

--- prob34.js
+++ prob34.js
@@ -3,13 +3,14 @@
 // received in a comma delimited string
 // Object.defineProperty(exports, "__esModule", { value: true });
 
-var input = "1,2,3,4,5,6,9"; // 15
+var input = "1,2,3,4,5,4.90"; // 15
 var sumNumberString = function (input) {
     var sepInput = input.split(",");
     var result = 0;
     for (var _i = 0, sepInput_1 = sepInput; _i < sepInput_1.length; _i++) {
         var i = sepInput_1[_i];
-        result += parseInt(i);
+        // result += parseInt(i);
+        result += parseFloat(i);
     }
     return result;
 };
