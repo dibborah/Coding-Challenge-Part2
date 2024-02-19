c260606075b3053a5378458dd09d0c54d1e54b20
dibborah <dibyajyotiborah469@gmail.com>
2024-02-19 19:28:46 +0530

    starting csvToArray problem

---
+++ prob36.ts
@@ -0,0 +1,16 @@
+// Create a function to convert 
+// a CSV text to a “bi-dimensional” array
+
+// Example usage
+const csv = `Name,Age,Email
+John,25,john@example.com
+Alice,30,alice@example.com
+Bob,35,bob@example.com`;
+
+// const getWords(text)
+// text => line 
+
+const csvToArray = (text: string): string[] => {
+
+    return [text];
+}
