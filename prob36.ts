126c24e524a8e14a96129672e8d9b2f43fed1be0
dibborah <dibyajyotiborah469@gmail.com>
2024-02-20 11:18:08 +0530

    solving the csvToArray

--- prob36.ts
+++ prob36.ts
@@ -1,4 +1,4 @@
-// Create a function to convert 
+// Create a function to convert
 // a CSV text to a “bi-dimensional” array
 
 // Example usage
@@ -8,11 +8,26 @@ Alice,30,alice@example.com
 Bob,35,bob@example.com`;
 
 // const getWords(text)
-// text => line 
+// text => line
+
+const separatorWhenSpace = (text: string) => {
+  const separator = [" ", "\t"];
+  return separator.includes(text);
+};
+
+const separatorWhenComma = (text: string) => {
+  const separator = [","];
+  return separator.includes(text);
+};
 
 const csvToArray = (text: string): string[] => {
-
-    return [text];
-}
+  const arr: any = [];
+  for (let i = 0; i < text.length; i++) {
+    if (separatorWhenSpace(text)) {
+      arr.push(text.substring)
+    }
+  }
+  return [text];
+};
 
 // checkout push in main branch
